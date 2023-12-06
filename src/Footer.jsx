1de8afc082560266912c0a4c560d8e06b0550518
Jahana Sultan <91688645+JahanaSultan@users.noreply.github.com>
2023-12-06 09:51:21 +0400

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
     <ul>
-      <Link to="#"><li><i className="ri-instagram-line"></i></li></Link>
+      <Link to="https://www.instagram.com/whisperofverses/"><li><i className="ri-instagram-line"></i></li></Link>
       <Link to="#"><li><i className="ri-telegram-line"></i></li></Link>
     </ul> 
     <h4><i className="ri-copyright-line"></i> 2023</h4>
